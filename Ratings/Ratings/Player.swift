b86c8585636dc3dd3b26b16e7ef93a21652593e5
Angel Baek <angelbaek1@gmail.com>
2015-05-18 13:59:39 -0400

    create Player model. seed data with sample data of players

--- Ratings/Ratings/Player.swift
+++ Ratings/Ratings/Player.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+class Player: NSObject{
+//    Player is container object for three properties: player's name, game they're playing, and a rating of 1 to 5 stars
+    var name: String
+    var game: String
+    var rating: Int
+    
+    init(name: String, game: String, rating: Int) {
+        self.name = name
+        self.game = game
+        self.rating = rating
+        super.init()
+    }
+}
+

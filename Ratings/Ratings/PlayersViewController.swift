4f1f6cae1bfc63abde656607dbbc629e3865bc88
Angel Baek <angelbaek1@gmail.com>
2015-05-20 15:59:56 -0400

    dequeue custom cells. images and labels looked up by tag on cell.populated with data from player obj

--- Ratings/Ratings/PlayersViewController.swift
+++ Ratings/Ratings/PlayersViewController.swift
@@ -23,13 +23,41 @@ class PlayersViewController: UITableViewController {
         return players.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-
-        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) as! UITableViewCell
-        let player = players[indexPath.row] as Player
-        cell.textLabel?.text = player.name
-        cell.detailTextLabel?.text = player.game
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+//      // UITableView.style = subtitle
+//        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) as! UITableViewCell
+//        let player = players[indexPath.row] as Player
+//        cell.textLabel?.text = player.name
+//        cell.detailTextLabel?.text = player.game
+//        return cell
+//        
+       // method dequeueReusableCellWithIdentifier(_:forIndexPath:) will check to see if there is an existing cell that can be recycled.
+       // if not, it will automatically allocate a prototype cell and return it to you.
+       // remember to reuse the identifier that you set up on the prototype cell in the storyboard editor -- in this case PlayerCell
+//    
+//    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) as! UITableViewCell //1
+        
+        let player = players[indexPath.row] as Player //2
+        
+        if let nameLabel = cell.viewWithTag(100) as? UILabel { //3
+            nameLabel.text = player.name
+        }
+        if let gameLabel = cell.viewWithTag(101) as? UILabel {
+            gameLabel.text = player.game
+        }
+        if let ratingImageView = cell.viewWithTag(102) as? UIImageView {
+            ratingImageView.image = self.imageForRating(player.rating)
+        }
         return cell
+
+        
+        // BREAKDOWN:
+        //  1. dequeueReusableCellWithIdentifier will dequeue an existing cell with the reuse identifier PlayerCell if available or create a new one if not.
+        //  2. You look up the Player object corresponding to the row being populated and assign it to player.
+        //  3. The labels and images are looked up by their tags on the cell (custom cell; cannot use UITableViewCell's textLabel and detailLabel's properties. MUST USE TAGS.)
         
     }
     
@@ -54,9 +82,6 @@ class PlayersViewController: UITableViewController {
 }
 
 // NOTES:
-// 1. method dequeueReusableCellWithIdentifier(_:forIndexPath:) will check to see if there is an existing cell that can be recycled.
-// if not, it will automatically allocate a prototype cell and return it to you.
-// remember to reuse the identifier that you set up on the prototype cell in the storyboard editor -- in this case PlayerCell
 
 // 2. Note: In this app you’re using only one prototype cell but if your table needs to display different kinds of cells then you can simply add additional prototype cells to the storyboard. You can either duplicate the existing cell to make a new one, or increment the value of the Table View’s Prototype Cells attribute. Be sure to give each cell its own re-use identifier, though.
 

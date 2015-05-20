67e3062f6c6d6dbb580e3ee0a606b3d0ebcd3c23
Angel Baek <angelbaek1@gmail.com>
2015-05-20 16:35:18 -0400

    create PlayerCell

--- Ratings/Ratings/PlayerCell.swift
+++ Ratings/Ratings/PlayerCell.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 
-class PlayerCell: UITableViewController {
+class PlayerCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
 
 }

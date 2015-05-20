366e17aa3d43433cae8207f8d5e138c5e51035f3
Angel Baek <angelbaek1@gmail.com>
2015-05-20 16:10:17 -0400

    stop using tags to access labels. connect labels to outlets instead

---
+++ Ratings/Ratings/PlayerCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerCell.swift
+//  Ratings
+//
+//  Created by Angel Baek on 5/20/15.
+//  Copyright (c) 2015 Angel Baek. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewController {
+
+}
